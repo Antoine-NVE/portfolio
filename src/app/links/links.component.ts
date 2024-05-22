db3a801a4dbe1816df8cd510be9e0dc146617b94
Antoine-NVE <antoinenavette@gmail.com>
2024-05-22 16:27:32 +0200

    Légères modifications de style

--- src/app/links/links.component.ts
+++ src/app/links/links.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-links',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './links.component.html',
   styleUrl: './links.component.css'
 })
